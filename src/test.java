b892f4acf721a70401b1eceeab646832c22c77d4
thinking-aloud <lennart.k@thinking-aloud.eu>
2012-11-09 22:50:33 +0100

    h

---
+++ src/test.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: lennart
+ * Date: 11/9/12
+ * Time: 10:49 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class test
+{
+}
